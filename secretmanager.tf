882678ad908c5898bed5201c7c8058ecbe6ca149
Nathan <106090674+SirNatan1@users.noreply.github.com>
2023-09-07 13:20:43 +0300

    Update secretmanager.tf
    Added data source secret (pulling secrets that were created manually)

--- secretmanager.tf
+++ secretmanager.tf
@@ -12,5 +12,16 @@ resource "aws_secretsmanager_secret_version" "secrets" {
   depends_on = [aws_secretsmanager_secret.secret_map]
 }
 
+// pulling existing secret that was created manually
+# this part will pull the secret
+data "aws_secretsmanager_secret" "existing_secret" {
+  arn = "<arn-of-the-secret>"
+}
+
+# this part pulls the value of the secret
+data "aws_secretsmanager_secret_version" "existing_secret" {
+  secret_id = data.aws_secretsmanager_secret.existing_secret.id
+}
+
 ## dont forget to add in variables file the var: secrets_map
 ## the env var should look as such:  export TF_VAR_secrets_map='{"key":"value","key2":"value2"}'
